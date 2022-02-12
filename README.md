280a6474a0c5fdba201601e22a506d3a20c341e1
Sameer Singh <sameeersingh@gmail.com>
2022-02-12 00:07:09 -0800

    complete initial implementation: supports multiple leaderboards and sorting

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # maestro-leaderboard
-Server for rendering the results for the Maestro course
+Server for rendering the results for the Maestro course.
+
+## Server Configuration
+
+Stored in `config.json`, and specifies server address, and details common to all leaderboards:
+```json
+{
+    "ip": "0.0.0.0",
+    "port": "443",
+    "subtitle": "Leaderboard for the CS175 AI Project Course",
+    "data_path": "./data/" # stores the leaderboard data
+}
+```
+
+## Leaderboard
+
+Each leaderboard is specified using an `ID`, such as `attack-homework`. This `ID` becomes specifies the URL of the leaderboard, i.e. `/leaderboard/<ID>`, such as `/leaderboard/attack-homework`.
+
+Each leaderboard is specified using two files (in `data_path`):
+
+- **Configuration** (`<ID>-config.yml`): A YAML file that specifies information about the leaderboard, such as:
+  ```yaml
+  title: Attack Project # Name of Leaderboard
+  sort_cols: # Column names to show as candidates for sorting (first one is used as default)
+    - Targeted
+    - Untargeted
+  table_name_column: Type # (optional) Column to use to split into multiple tables
+  table_names: # (needed if splitting) Mapping of column values to readable names of the tables
+    black: "Black-box Attacks"
+    white: "White-box Attacks"
+  ``` 
+- **Data** (`<ID>.csv`): A CSV file (with headers) that store the data to be displayed. Should contain at least `sort_cols` and `table_name_column` (if splitting into multiple files). Example data:
+  ```csv
+  Zhou,10.0,100.0,white
+  Sergio,100.0,10.0,white
+  Sameer,5.0,5.0,black
+  Mani,95.0,95.0,black
+  ```
+## Run the Server
+
+To install the requirements:
+```bash
+pip install -r requirements.txt
+```
+
+To run the server:
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,103 @@
+from tokenize import String
+from typing import Dict
+import flask
+from flask import request, jsonify
+from flask import Flask, render_template
+import json
+from os import path
+import yaml
+import pandas as pd
+
+server_config_file = "config.json"
+with open(server_config_file, "r") as f:
+    server_config = json.load(f)
+IP_ADDR = server_config["ip"]
+PORT = server_config["port"]
+
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+class Leaderboard:
+    """A single leaderboard page"""
+
+    @staticmethod
+    def config_file(id): return f"{server_config['data_path']}/{id}-config.yml"
+
+    @staticmethod
+    def data_file(id): return f"{server_config['data_path']}/{id}.csv"
+
+    @classmethod
+    def check(cls, id):
+        """Checks whether the leaderboard exists"""
+        return path.exists(cls.config_file(id)) and path.exists(cls.data_file(id)) 
+
+    def __init__(self, id):
+        self.id = id
+        self.title = None
+        self.results: Dict[String,pd.DataFrame] = None
+        self.table_name_column = None
+        self.sort_cols = []
+        self.table_names = {}
+    
+    def load_config(self):
+        with open(self.config_file(self.id), "r") as f:
+            config = yaml.safe_load(f)
+            self.title = config['title']
+            self.sort_cols = config['sort_cols']
+            if "table_name_column" in config:
+                self.table_name_column = config['table_name_column']
+                self.table_names = config['table_names']
+
+    def load_data(self):
+        self.results = {}
+        table = pd.read_csv(Leaderboard.data_file(self.id))
+        self.tables = {}
+        if self.table_name_column is None:
+            self.tables[""] = table
+        else:
+            for value_id, table_name in self.table_names.items():
+                subtable = table[table[self.table_name_column] == value_id]
+                subtable = subtable.drop(columns=[self.table_name_column])
+                self.tables[table_name] = subtable
+
+    def get_render_data(self, sortby=None):
+        render_data = {}
+        render_data['title'] = self.title
+        render_data['tables'] = {}
+        render_data['sort_col'] = sortby if sortby else self.sort_cols[0]
+        render_data['sort_cols'] = self.sort_cols
+        for table_id, table in self.tables.items():
+            render_data['tables'][table_id] = table.sort_values(by=render_data['sort_col'], ascending=False)
+        return render_data
+
+def main():
+    @app.route("/", methods=["GET"])
+    def home():
+        return "<h1>The Home of Maestro Server</p>"
+
+    @app.route("/leaderboard/<leaderboard_id>")
+    def leaderboard(leaderboard_id):
+        args = request.args
+        sortby = args.get("sortby")
+        if Leaderboard.check(leaderboard_id):
+            leaderboard = Leaderboard(leaderboard_id)
+            leaderboard.load_config()
+            leaderboard.load_data()
+            return render_template(
+                "index.html",
+                data=leaderboard.get_render_data(sortby),
+                server=server_config
+            )
+        else:
+            return f"<h1>Cannot find leaderboard with id {leaderboard_id}</p>"
+
+    # ------------------ END ATTACK SERVER FUNCTIONS ---------------------------
+
+    print("Server Running...........")
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=4999)
+    app.run(host=IP_ADDR, port=PORT)
+
+
+if __name__ == "__main__":
+    main()
